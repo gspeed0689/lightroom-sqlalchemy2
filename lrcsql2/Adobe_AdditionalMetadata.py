d88c2bfec745a33856e2f51fea7f6613b2855a78
Garrett Speed <6378547+gspeed0689@users.noreply.github.com>
2024-01-22 20:27:50 +0100

    Created table of tables (tabletable.csv)

--- lrcsql2/Adobe_AdditionalMetadata.py
+++ lrcsql2/Adobe_AdditionalMetadata.py
@@ -2,19 +2,19 @@ from base import Base
 
 from sqlalchemy.orm import mapped_column, Mapped
 from sqlalchemy import Text, Integer, Float, DateTime, Date, Time, Boolean, Uuid
-from sqlalchemy import BLOB
+from sqlalchemy import BLOB, 
 
 from datetime import datetime
 
 class Adobe_AdditionalMetadata(Base):
 	__tablename__ = "Adobe_AdditionalMetadata"
-	id_local: Mapped[] = mapped_column(primary_key=True)
-	id_global: Mapped[] = mapped_column(nullable=False, unique=True)
-	additionalInfoSet: Mapped[] = mapped_column(nullable=False)
-	embeddedXmp: Mapped[] = mapped_column(nullable=False)
-	externalXmpIsDirty: Mapped[] = mapped_column(nullable=False)
-	image: Mapped[] = mapped_column()
-	incrementalWhiteBalance: Mapped[] = mapped_column(nullable=False)
+	id_local: Mapped[int] = mapped_column(Integer, primary_key=True)
+	id_global: Mapped[str] = mapped_column(Uuid, nullable=False, unique=True)
+	additionalInfoSet: Mapped[int] = mapped_column(nullable=False)
+	embeddedXmp: Mapped[int] = mapped_column(nullable=False)
+	externalXmpIsDirty: Mapped[int] = mapped_column(nullable=False)
+	image: Mapped[int] = mapped_column()
+	incrementalWhiteBalance: Mapped[int] = mapped_column(nullable=False)
 	internalXmpDigest: Mapped[] = mapped_column()
 	isRawFile: Mapped[] = mapped_column(nullable=False)
 	lastSynchronizedHash: Mapped[] = mapped_column()
